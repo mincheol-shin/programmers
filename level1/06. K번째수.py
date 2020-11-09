82b607d3d70f97b8c1827216e07d84fdb968741d
Mincheol Shin <55150540+mincheol-shin@users.noreply.github.com>
2020-11-09 22:11:16 +0900

    Add exercise

---
+++ level1/06. K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        result = sorted(array[command[0] - 1:command[1]])
+        answer.append(result[command[2] - 1])
+
+    return answer
