43c2334247da357da504396dee0685f514defab2
Mincheol Shin <55150540+mincheol-shin@users.noreply.github.com>
2020-12-30 17:29:19 +0900

    Add exercise

--- level2/06. 다리를 지나는 트럭.py
+++ level2/06. 다리를 지나는 트럭.py
@@ -2,29 +2,19 @@ from collections import deque
 
 
 def solution(bridge_length, weight, truck_weights):
-    trucks = deque(truck_weights)
-    truck_on_bridge = deque()
-    current_weight = 0
-    time = 1
-    passing_truck = 0
-    while passing_truck != len(truck_weights):
-        if len(trucks) and current_weight + trucks[0] <= weight:
-            current_weight += trucks[0]
-            truck_on_bridge.append({trucks.popleft(): 0})
+    bridge = [0] * bridge_length
+    time = 0
+    bridge_weight = 0
 
-        for i in range(0, len(truck_on_bridge)):
-            key = list(truck_on_bridge[i].keys())[0]
-            if truck_on_bridge[i][key] == -1:
-                continue
-
-            else:
-                truck_on_bridge[i][key] += 1
-
-                if truck_on_bridge[i][key] == bridge_length:
-                    truck_on_bridge[i][key] = -1
-                    current_weight -= key
-                    passing_truck += 1
-
+    while bridge:
+        bridge_weight -= bridge[0]
+        bridge.pop(0)
+        if len(truck_weights) > 0 and bridge_weight + truck_weights[0] <= weight:
+            bridge_weight += truck_weights[0]
+            bridge.append(truck_weights.pop(0))
+        elif truck_weights:
+            bridge.append(0)
         time += 1
-
     return time
+
+
